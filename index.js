a7205289333e654fe0aa48203d3c407d765a15d8
Rebekah <figueroarebekah@gmail.com>
2023-01-09 10:21:11 -0600

    commit tree 20801d73e3952abec9c9b88dde6ff8eb06b28f0d

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+
+// function submitData(name, email) {
+// 	return fetch("http://localhost:3000/users", {
+// 		method: "POST",
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 			Accept: "application/json",
+// 		},
+// 		body: JSON.stringify({
+// 			name,
+// 			email,
+// 		}),
+// 	});
+// 	.then((response) => response.json())
+// 	.then((object) => {
+// 		document.body.innerHTML = object.id;
+// 	})
+// 	.catch((error) => {
+// 		document.body.innerHTML = error.message;
+// 	});
+// }
+
+function submitData(name, email) {
+	const formData = {
+		name: name,
+		email: email,
+	};
+
+	const configurationObject = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(formData),
+	};
+
+	return fetch("http://localhost:3000/users", configurationObject)
+		.then((response) => response.json())
+		.then((object) => {
+			document.body.innerHTML = object.id;
+		})
+		.catch((error) => {
+			document.body.innerHTML = error.message;
+		});
+}
